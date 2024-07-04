6b7a5773089c196f027419b19cafab8989fb1a22
vinisst <134705237+vinisst@users.noreply.github.com>
2024-07-04 09:14:11 -0300

    Add files via upload

---
+++ som.js
@@ -0,0 +1,77 @@
+var a = new Audio(); 
+a.src = "A.mp3";
+
+var b = new Audio(); 
+b.src = "B.mp3";
+
+var c = new Audio(); 
+c.src = "C.mp3";
+
+var d = new Audio(); 
+d.src = "D.mp3";
+
+var e = new Audio(); 
+e.src = "E.mp3";
+
+var f = new Audio(); 
+f.src = "F.mp3";
+
+var g = new Audio(); 
+g.src = "G.mp3";
+
+var h = new Audio(); 
+h.src = "H.mp3";
+
+var i = new Audio(); 
+i.src = "I.mp3";
+
+var j = new Audio(); 
+j.src = "J.mp3";
+
+var k = new Audio(); 
+k.src = "K.mp3";
+
+var l = new Audio(); 
+l.src = "L.mp3";
+
+var m = new Audio(); 
+m.src = "M.mp3";
+
+var n = new Audio(); 
+n.src = "N.mp3";
+
+var o = new Audio(); 
+o.src = "O.mp3";
+
+var p = new Audio(); 
+p.src = "P.mp3";
+
+var q = new Audio(); 
+q.src = "Q.mp3";
+
+var r = new Audio(); 
+r.src = "R.mp3";
+
+var s = new Audio(); 
+s.src = "S.mp3";
+
+var t = new Audio(); 
+t.src = "T.mp3";
+
+var u = new Audio(); 
+u.src = "U.mp3";
+
+var v = new Audio(); 
+v.src = "V.mp3";
+
+var w = new Audio(); 
+w.src = "W.mp3";
+
+var x = new Audio(); 
+x.src = "X.mp3";
+
+var y = new Audio(); 
+y.src = "Y.mp3";
+
+var z = new Audio(); 
+z.src = "Z.mp3";
